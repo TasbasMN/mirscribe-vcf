c603a11e611b0a3ede0727b87ec7800a3de62200
“Nazif <nazifts@gmail.com>
2024-04-23 21:32:40 +0300

    remove parse_cli_arguments()

--- scripts/truba_operations.py
+++ scripts/truba_operations.py
@@ -1,18 +1,4 @@
-import logging, argparse, psutil, csv
-
-
-def parse_cli_arguments():
-    """Parse command-line arguments."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument("vcf", help="VCF file to analyze")
-    parser.add_argument("-s", "--start", default=0, type=int, help="start index")
-    parser.add_argument("-e", "--end", default=-1, type=int, help="end index")
-    parser.add_argument("-o", "--output_dir", default="./results", help="Output directory for runtime file, defaults to ./results")
-    parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose logging")
-    parser.add_argument("-p", "--profile", action='store_true', help='Enable profiler')
-    return parser.parse_args()
-
-
+import logging, psutil, csv
 
 
 def print_memory_usage(verbose=False):
