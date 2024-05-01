00b0a65cfb46975404c71d4787fa42d87231b885
“Nazif <nazifts@gmail.com>
2024-05-01 13:53:35 +0300

    remove redundant timing code

--- main_v3.py
+++ main_v3.py
@@ -9,15 +9,7 @@ import json
 
 
 def time_it(func):
-    """
-    Decorator function that measures the execution time, memory usage, and system load average of a given function.
 
-    Parameters:
-        func (callable): The function to be timed.
-
-    Returns:
-        callable: The wrapped function that measures the execution time, memory usage, and system load average.
-    """
     def wrapper(*args, **kwargs):
         process = psutil.Process()
 
@@ -74,48 +66,8 @@ def time_it(func):
     return wrapper
 
 
-def zip_csv_files_with_vcf_id(directory: str, vcf_id: str, archive_name: str):
-    """
-    Compress all CSV files in the specified directory that start with the given vcf_id into a ZIP file.
-
-    Parameters:
-    - directory: The directory containing the CSV files to compress.
-    - vcf_id: The specific prefix that the CSV files should start with to be included in the ZIP.
-    - archive_name: The name of the resulting ZIP archive file.
-
-    Returns:
-    None
-    """
-    # Create a full path for the archive
-    archive_path = os.path.join(directory, archive_name)
-
-    # Create a new ZIP file
-    with zipfile.ZipFile(archive_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
-        # Iterate over all the files in the directory
-        for filename in os.listdir(directory):
-            # Check if the file is a CSV and starts with the vcf_id
-            if filename.endswith('.csv') and filename.startswith(vcf_id):
-                # Create the full file path
-                file_path = os.path.join(directory, filename)
-                # Add file to the ZIP file
-                zipf.write(file_path, filename)
-
-    print(
-        f"CSV files starting with {vcf_id} have been compressed into {archive_path}")
-
-
 def stitch_csv_files(output_dir: str, final_output_filename: str):
-    """
-    Stitch together all CSV files in the specified output directory into a single CSV file
-    without loading them all into memory at once.
 
-    Parameters:
-    - output_dir: The directory where the CSV files are stored.
-    - final_output_filename: The name of the final stitched CSV file.
-
-    Returns:
-    None
-    """
     # List all CSV files in the output directory that match the pattern
     csv_files = [f for f in os.listdir(
         output_dir) if f.endswith('.csv') and 'result_' in f]
@@ -151,16 +103,7 @@ def stitch_csv_files(output_dir: str, final_output_filename: str):
 
 
 def remove_small_stitched_files(output_dir: str, exclude_filename: str):
-    """
-    Remove smaller stitched CSV files from the output directory, excluding the final stitched file.
 
-    Parameters:
-    - output_dir: The directory where the CSV files are stored.
-    - exclude_filename: The name of the final stitched CSV file to exclude from deletion.
-
-    Returns:
-    None
-    """
     # List all files in the output directory that match the pattern and are not the final stitched file
     files_to_remove = [f for f in os.listdir(output_dir) if f.startswith(
         'result_') and f.endswith('.csv') and f != exclude_filename]
@@ -174,15 +117,7 @@ def remove_small_stitched_files(output_dir: str, exclude_filename: str):
 
 
 def delete_fasta_files(directory: str):
-    """
-    Delete all FASTA (.fa) files in the specified directory.
 
-    Parameters:
-    - directory: The directory to search for FASTA files.
-
-    Returns:
-    None
-    """
     # List all files in the directory that end with .fa
     fasta_files = [f for f in os.listdir(directory) if f.endswith('.fa')]
 
@@ -193,22 +128,12 @@ def delete_fasta_files(directory: str):
 
     print("All FASTA files have been deleted.")
 
+
 def zip_files(folder_path, prefix, extension):
-    """
-    Creates a ZIP archive containing files in the specified folder
-    that match the given prefix and extension.
 
-    Args:
-        folder_path (str): The path to the folder containing the files.
-        prefix (str): The prefix to match in the file names.
-        extension (str): The file extension to match (e.g., ".txt", ".py").
-
-    Returns:
-        tuple: A tuple containing the full path to the created ZIP archive file
-               and a list of filenames that were zipped.
-    """
     if not os.path.isdir(folder_path):
-        raise ValueError(f"The folder path provided does not exist: {folder_path}")
+        raise ValueError(
+            f"The folder path provided does not exist: {folder_path}")
 
     zip_filename = f"{prefix}_awk_outputs.zip"
     zip_filepath = os.path.join(folder_path, zip_filename)
@@ -226,17 +151,12 @@ def zip_files(folder_path, prefix, extension):
         os.remove(zip_filepath)
         return None, []
     
+    print(f"Files zipped successfully: {zip_filepath}")
     return os.path.abspath(zip_filepath), zipped_files
 
-def delete_zipped_files(zip_file_path, zipped_files):
-    """
-    Deletes the original files that were zipped, but only if the ZIP file
-    was successfully created and is not empty.
 
-    Args:
-        zip_file_path (str): The full path to the created ZIP archive file.
-        zipped_files (list): A list of filenames that were zipped.
-    """
+def delete_zipped_files(zip_file_path, zipped_files):
+
     if not os.path.isfile(zip_file_path) or os.path.getsize(zip_file_path) == 0:
         print(f"ZIP file not created or empty: {zip_file_path}")
         return
@@ -246,24 +166,23 @@ def delete_zipped_files(zip_file_path, zipped_files):
         file_path = os.path.join(folder_path, file)
         if os.path.isfile(file_path):
             os.remove(file_path)
-    
-    print(f"Original files deleted after successful ZIP creation: {zip_file_path}")
+
+    print(
+        f"Original files deleted after successful ZIP creation: {zip_file_path}")
+
 
 @time_it
 def process_chunk(chunk: pd.DataFrame, start_index: int, end_index: int, output_dir: str, vcf_id: str) -> tuple:
-    start_time = datetime.now()
-    # Perform your analysis pipeline on the chunk
+
     result = analysis_pipeline(
         chunk, start_index, end_index, output_dir, vcf_id)
-    end_time = datetime.now()
-    processing_time = end_time - start_time
 
     # Write the result to a CSV file in the output directory
     result_file = os.path.join(
         output_dir, f'result_{start_index}_{end_index}.csv')
     result.to_csv(result_file, index=False)
 
-    return start_index, end_index, processing_time
+    return start_index, end_index
 
 
 def analysis_pipeline(df: pd.DataFrame, start_index: int, end_index: int, output_dir: str, vcf_id: str, verbose: bool = False,) -> pd.DataFrame:
@@ -305,13 +224,7 @@ def analysis_pipeline(df: pd.DataFrame, start_index: int, end_index: int, output
 @time_it
 def run_vcf_analysis(vcf_full_path: str, chunksize: int, output_dir: str, vcf_id: str):
 
-    # todo remove this timing code
-    total_start_time = datetime.now()
-    report_file = os.path.join(output_dir, 'analysis.csv')
-
-    with ThreadPoolExecutor(max_workers=WORKERS) as executor, open(report_file, 'w') as report:
-        report.write(f"start_index,end_index,time_elapsed\n")
-        report.write(f"start,time,{total_start_time}\n")
+    with ThreadPoolExecutor(max_workers=WORKERS) as executor:
 
         futures = []
         start_index = 0
@@ -323,15 +236,7 @@ def run_vcf_analysis(vcf_full_path: str, chunksize: int, output_dir: str, vcf_id
             start_index = end_index + 1
 
         for future in as_completed(futures):
-            start_index, end_index, processing_time = future.result()
-            report.write(f"{start_index},{end_index},{processing_time}\n")
-
-    total_end_time = datetime.now()
-    total_processing_time = total_end_time - total_start_time
-
-    with open(report_file, 'a') as report:
-        report.write(f"end,time,{total_end_time}\n")
-        report.write(f"total,time: {total_processing_time}\n")
+            start_index, end_index = future.result()
 
 
 def main():
@@ -374,9 +279,10 @@ def main():
     stitch_csv_files(OUTPUT_DIR, results_filename)
     remove_small_stitched_files(OUTPUT_DIR, results_filename)
     delete_fasta_files(OUTPUT_DIR)
-    
+
     zip_file_path, zipped_files = zip_files(OUTPUT_DIR, "rnad", ".csv")
     delete_zipped_files(zip_file_path, zipped_files)
+
 
 if __name__ == '__main__':
     main()
