5ce2cd13976434e9fe2c08cd92a7dc679c06aa54
tasbasmn <nazifts@gmail.com>
2024-08-09 18:17:45 +0300

    add memory profiling to synth py

--- synth.py
+++ synth.py
@@ -1,13 +1,19 @@
 import os
 from scripts.main_operations import *
 from scripts.config import *
+import time
+from memory_profiler import profile
 
-
+@profile
 def main():
 
     # Create the output directory if it doesn't exist
     os.makedirs(OUTPUT_DIR, exist_ok=True)
 
+    # print and save current time to a variable
+    start = time.time()
+    
+    
     run_pipeline(VCF_FULL_PATH, CHUNKSIZE, OUTPUT_DIR, VCF_ID)
     print("Processing completed. Results saved in the output directory.")
 
@@ -19,6 +25,8 @@ def main():
     zip_file_path, zipped_files = zip_files(OUTPUT_DIR, "rnad", ".csv")
     delete_zipped_files(zip_file_path, zipped_files)
 
-
+    end = time.time()
+    print(f"Total runtime: {end - start:.2f} seconds")
+    
 if __name__ == '__main__':
     main()
