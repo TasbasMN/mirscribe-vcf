809db20e1dc20aa24b8353345d74bfd5461858d8
tasbasmn <nazifts@gmail.com>
2024-09-08 23:19:34 +0300

    add script that creates 560 vcfs from big file

---
+++ create_560.py
@@ -0,0 +1,37 @@
+import os
+import pandas as pd
+
+df = pd.read_csv("560.txt", sep="\t")
+cols_to_drop = ["Project", "ID", "Genome", "mut_type", "Type"]
+df = df.drop(columns=cols_to_drop)
+
+# setting vcfs to exclude
+target_folder = "data/sample_vcfs"
+vcfs_to_exclude = {os.path.basename(x).split(".")[0] for x in os.listdir(target_folder) if x.startswith("PD")}
+
+output_dir = "data/560"
+os.makedirs(output_dir, exist_ok=True)
+
+for name, group in df.groupby("Sample"):
+    if name in vcfs_to_exclude:
+        continue
+    
+    # Create the appropriate directory
+    if name.startswith(tuple(f"PD{i}" for i in range(1, 10))):
+        sample_dir = os.path.join(output_dir, name[:3])
+        os.makedirs(sample_dir, exist_ok=True)
+    else:
+        sample_dir = output_dir
+    
+    # Save the VCF file
+    file_path = os.path.join(sample_dir, f"{name}.vcf")
+    group.to_csv(file_path, sep="\t", index=False, header=False)
+
+
+
+
+
+
+
+
+
